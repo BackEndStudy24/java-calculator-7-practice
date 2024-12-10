1994d790ec3c1d491151880ed51a2eda5734d1d5
Teddysir <sdzx0719@naver.com>
2024-12-10 19:27:34 +0900

    feat: 커스텀 구분자 모델 생성

---
+++ src/main/java/calculator/model/Delimiter.java
@@ -0,0 +1,13 @@
+package calculator.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Delimiter {
+
+    private final static String COMMA = ",";
+    private final static String COLON = ":";
+
+    private List<String> customDelimiter = new ArrayList<>();
+
+}
